19f2d386239f21c5c218156ff4f2d9467286e4e1
Alessio <antonini@di.unito.it>
2017-02-07 22:44:35 +0100

    preparazioen contenuti landingpage

--- www/js/controllers/LandingCtrl.js
+++ www/js/controllers/LandingCtrl.js
@@ -1,7 +1,8 @@
 angular.module('firstlife.controllers')
 
-    .controller('LandingCtrl',  ['$log','$scope', '$state', '$rootScope', '$ionicPopup', '$stateParams', '$location', '$ionicLoading', '$filter','$window', 'UserService', 'myConfig', 'MemoryFactory', function($log,$scope, $state, $rootScope, $ionicPopup, $stateParams, $location, $ionicLoading, $filter,$window, UserService, myConfig, MemoryFactory) {
+    .controller('LandingCtrl',  ['$log','$scope', '$state', '$translate', '$ionicPopup', '$stateParams', '$location', '$ionicLoading', '$filter','$window', 'UserService', 'myConfig', 'MemoryFactory', function($log,$scope, $state, $translate, $ionicPopup, $stateParams, $location, $ionicLoading, $filter,$window, UserService, myConfig, MemoryFactory) {
         $scope.defaults = myConfig;
+        $scope.currentLang = $translate.use();
         // check cambio di stato
         $scope.$on("$stateChangeSuccess", function(event, toState, toParams, fromState) {
             event.preventDefault();
