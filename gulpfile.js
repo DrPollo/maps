e3623db2d489d8aa2276f65c9cae59ef600ac2b7
Alessio <antonini@di.unito.it>
2017-04-06 15:18:33 +0200

    switch url al nuovo ambiente

--- gulpfile.js
+++ gulpfile.js
@@ -173,7 +173,7 @@ gulp.task('setupenv',function(){
         config.myConfig.dev = false;
     }else if(gutil.env.dev){
         console.log('env dev');
-        config.myConfig.api_base_domain = "api.dev.firstlife.di.unito.it/";
+        config.myConfig.api_base_domain = "api.fldev.di.unito.it/";
         config.myConfig.dev = true;
     }
 
