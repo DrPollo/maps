02cc08faf10e73147e788bf7710fc6f8f9340e01
Alessio <antonini@di.unito.it>
2017-05-12 19:08:03 +0200

    test https

--- www/js/app.js
+++ www/js/app.js
@@ -264,11 +264,12 @@ angular.module('firstlife', ['firstlife.config', 'firstlife.controllers', 'first
             var page = "templates/callback-page.html";
             var useHTTPS = window.location.href.indexOf('https') > -1;
             console.log('page',window.location.protocol.concat( "//" ,window.location.host,'/')+ page, 'https?',useHTTPS);
-            if (useHTTPS) {
-                return window.location.protocol.concat( "//" ,window.location.host,'/')+ page;
-            } else {
-                return page;
-            }
+            // if (useHTTPS) {
+                return 'https:'.concat( "//" ,window.location.host,'/')+ page;
+                // return window.location.protocol.concat( "//" ,window.location.host,'/')+ page;
+            // } else {
+            //     return page;
+            // }
         },
         // templateUrl: "templates/callback-page.html",
         reloadOnSearch: false,
