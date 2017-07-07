7ed1f369619a1b6dc456c28276a4ccbd66f73ee3
Alessio <antonini@di.unito.it>
2017-07-07 22:59:48 +0200

    fix errore login callback: routing to home

--- www/js/app.js
+++ www/js/app.js
@@ -158,8 +158,8 @@ angular.module('firstlife', ['firstlife.config', 'firstlife.controllers', 'first
                         event.preventDefault();
                         $state.go('app.maps',search_params);
                     }
-                    $log.debug('going home');
-                    $state.go('home',search_params);
+                    // $log.debug('going home');
+                    // if()$state.go('home',search_params);
                     break;
                 case 'app.editor':
                     if(authenticate && !AuthService.isAuth()){
